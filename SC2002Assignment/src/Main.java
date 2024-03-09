15392c89e4894b8d41a06502423baaf32f44b8f8
Cebelle <84433822+Cebelle1@users.noreply.github.com>
2024-03-09 22:40:40 +0800

    Add files via upload

---
+++ SC2002Assignment/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * This is the Main class of the app.
+ * It is the entry point for the entire app.
+ */
+public class Main {
+    public static void main(String[] args) {
+        App FOMSApp = new App();
+        FOMSApp.start();
+    }
+    
+}
