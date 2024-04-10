636b9f70645d1c1f9190106206d85927b2e1e963
NicoleKaira <nicoleimatong.05@gmail.com>
2024-04-10 23:56:34 +0800

    done with Edit staff list
    need to redo the routing from wat page to wat. I leave it as is first for testing purposes

--- SC2002Assignment/src/controller/AdminController.java
+++ SC2002Assignment/src/controller/AdminController.java
@@ -6,6 +6,7 @@ import controller.abstracts.AController;
 import view.AdminHomePageView;
 import model.AdminRole;
 import model.EmployeeHandler;
+import model.StaffRole;
 import model.abstracts.AEmployee;
 //=====for TESTING
 import model.payments.IPaymentProcessor;
@@ -18,6 +19,7 @@ public class AdminController extends AController {
     private AEmployee currentuser;
     int inputBranch;
     int inputRole;
+    String gender;
 
     public AdminController(AEmployee user) {
         this.currentuser = user;
@@ -39,63 +41,20 @@ public class AdminController extends AController {
                 break;
 
             case 1: // Edit Staff Accounts
-                // make changes here!!
-                /*
-                 * List<AEmployee> employees = EmployeeHandler.getAllUnsortedEmployees();
-                 * adminHomePageView.printAllStaff(employees);
-                 */
                 adminHomePageView.renderApp(1);
                 int choiceEdit = adminHomePageView.getInputInt("");
-                filterNavigate(choiceEdit);
+                editNavigate(choiceEdit);
+                // this.navigate(0); // remove this later after, this is use for testing
+                // purposes!!
                 break;
 
             case 2: // Display All Staff List in term of branch, role, gender, age
                 adminHomePageView.renderApp(2);
                 int choiceDisplay = adminHomePageView.getInputInt("");
-                switch (choiceDisplay) {
-                    case 1: // branch
-                        while (true) {
-                            inputBranch = adminHomePageView
-                                    .getInputInt("Select Branch Name as filter: (1)JP (2)JE (3)NTU:  ");
-                            if (inputBranch > 0 && inputBranch < 4) {
-                                break;
-                            }
-                            adminHomePageView.errorintinput();
-                        }
-                        List<AEmployee> filterbybranch = adminRole.EmpFilterByBranch(inputBranch);
-                        adminHomePageView.printFilterStaff(filterbybranch); // continue coding from end, do i filter by
-                                                                            // asking string or int?
-                        break;
-
-                    case 2: // role
-                        while (true) {
-                            inputRole = adminHomePageView
-                                    .getInputInt("Select Role Name as filter: (1)Staffs (2)Managers ");
-                            if (inputRole > 0 && inputRole < 3) {
-                                break;
-                            }
-                            adminHomePageView.errorintinput();
-                        }
-                        List<AEmployee> filterbyrole = adminRole.EmpFilterByRole(inputRole);
-                        adminHomePageView.printFilterStaff(filterbyrole);
-                        break;
-
-                    case 3: // gender
-                        String gender = adminHomePageView
-                                .getInputString("Enter M for male or F for female as filter:  ");
-                        List<AEmployee> filterByGender = adminRole.EmpFilterByGender(gender);
-                        adminHomePageView.printFilterStaff(filterByGender);
-                        break;
-
-                    case 4: // age
-                        int minAge = adminHomePageView.getInputInt("Enter Min Age as filter: ");
-                        int maxAge = adminHomePageView.getInputInt("Enter Max Age as filter: ");
-                        List<AEmployee> filterbyage = adminRole.EmpFilterByAge(minAge, maxAge);
-                        adminHomePageView.printFilterStaff(filterbyage);
-                        break;
-                }
-
+                displayNavigate(choiceDisplay);
+                this.navigate(0); // remove this later after, this is use for testing
                 // use Employeefilter to print according to the filter
+                break;
 
             case 3: // Assign Managers
                 break;
@@ -121,16 +80,107 @@ public class AdminController extends AController {
 
     }
 
-    public void filterNavigate(int num) {
+    // =============================navigate for edit staff
+    // list===================================
+    public void editNavigate(int num) {
         switch (num) {
-            case 1: // filter by branch
+            case 1: // Add staff account if there an STAFFID already existing it will return
+                    // "StaffID already exist"
+                adminHomePageView.renderApp(3);
+                String name = adminHomePageView.getInputString("Enter new Staff's Name: ");
+                String staffID = adminHomePageView.getInputString("Enter new Staff's StaffID: ");
+                String role = adminHomePageView.getInputString("Enter new Staff's Role: ");
+                String gender = adminHomePageView.getInputString("Enter new Staff's Gender(M or F): ");
+                int age = adminHomePageView.getInputInt("Enter new Staff's Age: ");
+                String branch = adminHomePageView.getInputString("Enter new Staff's Branch: ");
+                String password = "password";// default password for new users is "password"
+                AEmployee newStaffAcc = new AdminRole(name, staffID, role, gender, age, branch, password);
+                adminRole.addStaff(newStaffAcc);
+                // this.navigate(2); // go back to edit account
+                this.navigate(1);
+                break;
 
-            case 2: // filter by role
+            case 2: // Remove Staff account
+                String staffNameToRemove = adminHomePageView.getInputString("Enter Staff Name to remove: ");
+                adminRole.removeStaff(staffNameToRemove);
+                this.navigate(1);
+                break;
 
-            case 3: // filter by gender
+            case 3: // edit staff account
+                String staffNameToEdit = adminHomePageView.getInputString("Enter Staff Name to edit: ");
+                adminRole.removeStaff(staffNameToEdit);
+                String nameUpdate = staffNameToEdit;
+                String staffIDUpdate = adminHomePageView.getInputString("Enter Staff's StaffID: ");
+                String roleUpdate = adminHomePageView.getInputString("Enter Staff's Role: ");
+                String genderUpdate = adminHomePageView.getInputString("Enter Staff's Gender(M or F): ");
+                int ageUpdate = adminHomePageView.getInputInt("Enter Staff's Age: ");
+                String branchUpdate = adminHomePageView.getInputString("Enter Staff's Branch: ");
+                String passwordUpdate = adminHomePageView.getInputString("Enter Staff's new password: ");// default
+                                                                                                         // password
+                // for new users is
+                AEmployee EditStaffAcc = new AdminRole(nameUpdate, staffIDUpdate, roleUpdate, genderUpdate, ageUpdate,
+                        branchUpdate, passwordUpdate);
+                adminRole.addStaff(EditStaffAcc);
+                this.navigate(1);
+                break;
 
-            case 4: // filter by age
+        }
 
+    }
+
+    // =============================navigate for display staff list with filters
+    // ===================================
+    public void displayNavigate(int num) {
+        switch (num) {
+            case 1: // branch
+                while (true) {
+                    inputBranch = adminHomePageView
+                            .getInputInt("Select Branch Name as filter: (1)JP (2)JE (3)NTU:  ");
+                    if (inputBranch > 0 && inputBranch < 4) {
+                        break;
+                    }
+                    adminHomePageView.renderApp(10);
+                }
+                List<AEmployee> filterbybranch = adminRole.EmpFilterByBranch(inputBranch);
+                adminHomePageView.printFilterStaff(filterbybranch); // continue coding from end, do i filter by
+                                                                    // asking string or int?
+                break;
+
+            case 2: // role
+                while (true) {
+                    inputRole = adminHomePageView
+                            .getInputInt("Select Role Name as filter: (1)Staffs (2)Managers ");
+                    if (inputRole > 0 && inputRole < 3) {
+                        break;
+                    }
+                    adminHomePageView.renderApp(10);
+                }
+                List<AEmployee> filterbyrole = adminRole.EmpFilterByRole(inputRole);
+                adminHomePageView.printFilterStaff(filterbyrole);
+                break;
+
+            case 3: // gender
+                while (true) {
+                    gender = adminHomePageView
+                            .getInputString("Enter M for male or F for female as filter:  ");
+                    if (gender == "M" || gender == "F" || gender == "f" || gender == "m") {
+                        break;
+                    }
+                    adminHomePageView.renderApp(10);
+                }
+                List<AEmployee> filterByGender = adminRole.EmpFilterByGender(gender);
+                adminHomePageView.printFilterStaff(filterByGender);
+                break;
+
+            case 4: // age
+                int minAge = adminHomePageView.getInputInt("Enter Min Age as filter: ");
+                int maxAge = adminHomePageView.getInputInt("Enter Max Age as filter: ");
+                List<AEmployee> filterbyage = adminRole.EmpFilterByAge(minAge, maxAge);
+                adminHomePageView.printFilterStaff(filterbyage);
+                break;
+            case 5: // print all no filter
+                List<AEmployee> allAEmployees = adminRole.EmpAllWithoutFilter();
+                adminHomePageView.printFilterStaff(allAEmployees); // print all emps to see the changes
         }
 
     }
--- SC2002Assignment/src/model/AdminRole.java
+++ SC2002Assignment/src/model/AdminRole.java
@@ -4,10 +4,14 @@ import model.payments.*;
 
 import java.util.List;
 
+import javax.print.DocFlavor.STRING;
+
+import controller.AdminController;
 import model.abstracts.AEmployee;
 
 public class AdminRole extends AEmployee {
     private PaymentMethodFactory paymentMethodFactory = new PaymentMethodFactory();
+    // AdminController adminController;
 
     public AdminRole(String Name, String StaffID, String Role, String Gender, int Age, String Branch, String Password) {
         super(Name, StaffID, Role, Gender, Age, Branch, Password);
@@ -16,6 +20,38 @@ public class AdminRole extends AEmployee {
     // Add your individual role method here like edit staffacc, display staff list
     // etc
     // create with i can get the fill Employee with no filter
+    // =======================================Edit (add, remove, edit) staff
+    // list======================================================
+    public void addStaff(AEmployee newAEmployee) {
+        int checker = DataManager.addNewStaffAccount(newAEmployee);
+        if (checker == -1) {
+            System.out.println("StaffID already exist");
+            System.out.println("Staff Account update list status: Not Successful");
+            return;
+        }
+        System.out.println("Staff Account update list status: Successful");
+
+        // System.out.println("staffID already exist.");
+    }
+
+    public void removeStaff(String staffNameToRemove) {
+        List<AEmployee> allAEmployees = EmpAllWithoutFilter();
+        for (AEmployee employee : allAEmployees) {
+            if (employee.getName().equals(staffNameToRemove)) {
+                DataManager.removeStaffAccount(staffNameToRemove);
+                // System.out.println("Staff account removed from staff list");
+                return;
+            }
+        }
+        // System.out.println("Staff Name Do Not exist");
+    }
+
+    // =======================================Display filter staff
+    // list======================================================
+    public List<AEmployee> EmpAllWithoutFilter() {
+        List<AEmployee> allAEmployees = EmployeeHandler.getAllUnsortedEmployees();
+        return allAEmployees;
+    }
 
     public List<AEmployee> EmpFilterByAge(int min, int max) {
         List<AEmployee> filter = EmployeeHandler.getAllUnsortedEmployees(); // create an object array of employees
